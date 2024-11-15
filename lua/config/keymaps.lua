74224e48953c760576cfd99a0a73669b28f12868
Alejandro <vaquerolopez.alejandro@gmail.com>
2024-11-15 23:27:19 +0000

    Configurated all languages

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local keymap = vim.keymap
-keymap.set("n", "<C-a>", "gg<S-v>G")
+keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all text" })
+keymap.set("n", "<leader>h", ":b#<CR>", { desc = "Back to prev buffer" })
