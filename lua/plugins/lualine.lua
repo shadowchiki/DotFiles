89440b875bd82ef781bf55244df08b71365a81c2
Alejandro <vaquerolopez.alejandro@gmail.com>
2024-11-16 09:15:07 +0000

    Configuring colorscheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,54 +1,54 @@
 return {
-  "nvim-lualine/lualine.nvim",
+	"nvim-lualine/lualine.nvim",
 
-  event = "VeryLazy",
-  opts = {
-    options = {
+	event = "VeryLazy",
+	opts = {
+		options = {
 
-      icons_enabled = true,
-      --theme = 'solarized_dark',
+			icons_enabled = true,
+			theme = "tokyonight",
 
-      section_separatos = { left = "", right = "" },
-      component_separators = { left = "", right = "" },
-      disabled_filetypes = {},
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_c = {
-        {
-          "filename",
-          file_status = true,
-          path = 0,
-        },
-      },
-      lualine_x = {
-        {
-          "diagnostics",
-          sources = { "nvim_diagnostic" },
-          symbols = { error = "  ", warn = " ", info = " ", hint = " " },
-        },
-        "encoding",
-        "filetype",
-      },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {
-        {
-          "filename",
-          file_status = true,
-          path = 1,
-        },
-      },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    tabline = {},
-    extensions = { "fugitive" },
-  },
+			section_separatos = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			disabled_filetypes = {},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch" },
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					path = 0,
+				},
+			},
+			lualine_x = {
+				{
+					"diagnostics",
+					sources = { "nvim_diagnostic" },
+					symbols = { error = "  ", warn = " ", info = " ", hint = " " },
+				},
+				"encoding",
+				"filetype",
+			},
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					path = 1,
+				},
+			},
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		extensions = { "fugitive" },
+	},
 }
